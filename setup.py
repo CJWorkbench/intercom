a3bd546edbe5a252c9cb16b6b25bb408a0c126bc
Adam Hooper <adam@adamhooper.com>
2019-08-07 14:35:29 -0400

    Require pandas 0.25.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(
-    name='intercom',
-    version='0.0.1',
-    description='Download user lists from Intercom',
-    author='Adam Hooper',
-    author_email='adam@adamhooper.com',
-    url='https://github.com/CJWorkbench/intercom',
-    packages=[''],
-    py_modules=['libraryofcongress'],
-    install_requires=['pandas==0.24.2', 'aiohttp==3.5.4']
+    name="intercom",
+    version="0.0.1",
+    description="Download user lists from Intercom",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/intercom",
+    packages=[""],
+    py_modules=["libraryofcongress"],
+    install_requires=["pandas==0.25.0", "aiohttp==3.5.4"],
 )
